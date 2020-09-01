6e27460202babc9e92f8ad00896e9f1951e2b2a8
Battinson <tonykarkovichpattinsonov@gmail.com>
2020-09-01 03:58:34 +0300

    add HMR

--- webpack-template/webpack/config/webpack.dev.js
+++ webpack-template/webpack/config/webpack.dev.js
@@ -1,5 +1,6 @@
 // Core
 import { merge } from "webpack-merge";
+import { HotModuleReplacementPlugin } from "webpack";
 
 // Constants
 import { DIST_DIR } from "../util/constants";
@@ -12,6 +13,9 @@ export default () =>
   merge(getCommonConfig(), {
     mode: "none",
     devtool: false,
+    entry: {
+      main: ["webpack-hot-middleware/client"],
+    },
     output: {
       filename: "[name].js",
       path: DIST_DIR,
@@ -19,5 +23,8 @@ export default () =>
     devServer: {
       contentBase: DIST_DIR,
       overlay: true,
+      hot: true,
+      open: true,
     },
+    plugins: [new HotModuleReplacementPlugin()],
   });
--- webpack-template/webpack/nodeAPI/start.js
+++ webpack-template/webpack/nodeAPI/start.js
@@ -2,6 +2,7 @@
 import webpack from "webpack";
 import express from "express";
 import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
 import chalk from "chalk";
 
 // Production Configuration
@@ -15,9 +16,11 @@ const compiler = webpack(getDevConfig());
 
 const app = express();
 const staticMiddleware = express.static(DIST_DIR);
-
 const devMiddleware = webpackDevMiddleware(compiler, getDevConfig().devServer);
+const hotMiddleware = webpackHotMiddleware(compiler);
+
 app.use(devMiddleware);
+app.use(hotMiddleware);
 app.use(staticMiddleware);
 
 app.listen(PORT, HOST, () =>
