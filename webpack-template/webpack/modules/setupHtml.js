f8904074737dddca0a01b8bbe24289c3a02ab909
Battinson <tonykarkovichpattinsonov@gmail.com>
2020-09-01 16:37:31 +0300

    add support for multiple html pages

--- webpack-template/webpack/modules/setupHtml.js
+++ webpack-template/webpack/modules/setupHtml.js
@@ -1,16 +1,25 @@
 // Core
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { readdirSync } from "fs";
 
 // Constants
 import { HTML_DIR, ASSETS_DIR } from "../util/constants";
 
+const pages = readdirSync(HTML_DIR);
+const plugins = pages.map(
+  (page) =>
+    new HtmlWebpackPlugin({
+      template: `${HTML_DIR}/${page}`,
+      title: `🚀 ${
+        page === "index.html" ? "Webpack" : page.replace(".html", "")
+      }`,
+      favicon: `${ASSETS_DIR}/images/webpack.ico`,
+      filename: page,
+    })
+);
+console.log(plugins);
+
 // Setup
 export const setupHtml = () => ({
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: `${HTML_DIR}/index.html`,
-      title: "🚀 Webpack",
-      favicon: `${ASSETS_DIR}/images/webpack.ico`,
-    }),
-  ],
+  plugins,
 });
